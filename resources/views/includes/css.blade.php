b5bf959fc93569f0172b4f366953d8a4227982f3
David Tobing <davidtopbing@gmail.com>
2018-01-16 22:03:54 +0700

    insert javascript and css

---
+++ resources/views/includes/css.blade.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ URL::asset('cs/my.min.css') }}">
---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,3 @@
+<p>
+    Navbar Works!
+</p>
